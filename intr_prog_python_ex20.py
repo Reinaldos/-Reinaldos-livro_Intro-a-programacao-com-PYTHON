3926e278b0a9fa54dd08093ea8bb6056369c601c
Reinaldos <crowlingster@gmail.com>
2022-01-06 10:22:14 -0300

    06/01/22

--- intr_prog_python_ex20.py
+++ intr_prog_python_ex20.py
@@ -2,6 +2,12 @@
 # um fumante. Pergunte a quantidade de cigarros fumados por dia e quantos anos 
 # ele já fumou. Considere que um fumante perde 10 minutos de vida a cada cigarro, 
 # calcule quantos dias de vida um fumante perderá. Exiba o total em dias.
-
+cigarrosdia = float(input('quantos cigarro tu fuma por dia meu cumpadi?'))
+cigarrosano = float(input('e há quantos anos tu fuma?'))
+minutoperdido = 10 * cigarrosdia
+horaperdida = minutoperdido * 60
+diasperdidos = horaperdida / 24
+anosperdidos = diasperdidos/360
+print('Voce fumou por %2.2d horas \n %2.2d dias \n e perdeu \n %2.2d anos com isso'%(horaperdida,diasperdidos,anosperdidos))
 
 
